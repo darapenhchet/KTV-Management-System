bed4f99c92a3639e5c57c5fa8fccb0c0ade1735f
DARAPENHCHET <darapenhchet@gmail.com>
2014-09-21 14:17:47 +0700

    Revert "commit"
    This reverts commit 8e4b327c2df47e7d2a01ac95c44e32e836459073.
    
    Conflicts:
    	KTVManagement/.svn/wc.db
    	KTVManagement/KTVManagement.v11.suo
    	KTVManagement/KTVManagement/Forms/Song/frmAddSong.Designer.vb
    	KTVManagement/KTVManagement/Forms/Song/frmAddSong.resx
    	KTVManagement/KTVManagement/KTVManagement.vbproj
    	KTVManagement/KTVManagement/My Project/Resources.Designer.vb
    	KTVManagement/KTVManagement/My Project/Resources.resx
    	KTVManagement/KTVManagement/bin/Debug/KTVManagement.application
    	KTVManagement/KTVManagement/bin/Debug/KTVManagement.exe
    	KTVManagement/KTVManagement/bin/Debug/KTVManagement.exe.manifest
    	KTVManagement/KTVManagement/bin/Debug/KTVManagement.pdb
    	KTVManagement/KTVManagement/bin/Debug/KTVManagement.vshost.application
    	KTVManagement/KTVManagement/bin/Debug/KTVManagement.vshost.exe.manifest
    	KTVManagement/KTVManagement/bin/Debug/KTVManagement.xml
    	KTVManagement/KTVManagement/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    	KTVManagement/KTVManagement/obj/Debug/KTVManagement.Resources.resources
    	KTVManagement/KTVManagement/obj/Debug/KTVManagement.application
    	KTVManagement/KTVManagement/obj/Debug/KTVManagement.exe
    	KTVManagement/KTVManagement/obj/Debug/KTVManagement.exe.manifest
    	KTVManagement/KTVManagement/obj/Debug/KTVManagement.pdb
    	KTVManagement/KTVManagement/obj/Debug/KTVManagement.vbproj.GenerateResource.Cache
    	KTVManagement/KTVManagement/obj/Debug/KTVManagement.xml
    	KTVManagement/KTVManagement/obj/Debug/TempPE/My Project.Resources.Designer.vb.dll

--- KTVManagement/KTVManagement/Forms/frmAboutus.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class frmAboutus
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        frmMainAdmin.Show()
-        Me.Close()
-    End Sub
-End Class
